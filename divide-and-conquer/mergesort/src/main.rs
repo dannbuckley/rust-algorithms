e22d09fd5888a4daf078e7a810d71b55a537a959
Daniel Buckley <buckley.daniel.n@gmail.com>
2020-04-25 17:25:32 -0600

    Mergesort and Quicksort

--- divide-and-conquer/mergesort/src/main.rs
+++ divide-and-conquer/mergesort/src/main.rs
@@ -1,3 +1,60 @@
+extern crate math;
+
+use math::round;
+use std::vec::Vec;
+
+// Sorts array a[0..n-1] by recursive mergesort
+// Input: An array a[0..n-1] of orderable elements
+// Output: Array a[0..n-1] sorted in nondecreasing order
+fn mergesort(a: &mut Vec<u8>) {
+    let n = a.len();
+    if n > 1 {
+        let nh: usize = round::floor(n as f64 / 2.0, 0) as usize;
+        let mut b = Vec::from(&a[0..nh]);        
+        let mut c = Vec::from(&a[nh..n]);
+        mergesort(&mut b);
+        mergesort(&mut c);
+        merge(&b, &c, a);
+    }
+}
+
+// Merges two sorted array into one sorted array
+// Input: Arrays b[0..p-1] and c[0..q-1] both sorted
+// Output: Sorted array a[0..p+q-1] of the elements of b and c
+fn merge(b: &Vec<u8>, c: &Vec<u8>, a: &mut Vec<u8>) {
+    let p = b.len();
+    let q = c.len();
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = 0;
+
+    while i < p && j < q {
+        if b[i] <= c[j] {
+            a[k] = b[i];
+            i = i + 1;            
+        } else {
+            a[k] = c[j];
+            j = j + 1;
+        }
+        k = k + 1;
+    }
+
+    if i == p {
+        for x in j..q {
+            a[(x - j) + k] = c[x];
+        }
+    } else {
+        for x in i..p {
+            a[(x - i) + k] = b[x];
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // sort example array from book
+    let mut a: Vec<u8> = vec![8, 3, 2, 9, 7, 1, 5, 4];
+    println!("Array before mergesort: {:?}", a);
+
+    mergesort(&mut a);    
+    println!("Array after mergesort: {:?}", a);
 }
--- divide-and-conquer/quicksort/src/main.rs
+++ divide-and-conquer/quicksort/src/main.rs
@@ -1,3 +1,67 @@
+use std::cmp::Ordering;
+use std::mem;
+
+// courtesy of https://stackoverflow.com/a/28294764
+fn swap<T>(x: &mut [T], i: usize, j: usize) {
+    let (lo, hi) = match i.cmp(&j) {
+        // no swapping necessary
+        Ordering::Equal => return,
+
+        // get the smallest and largest of the two indices
+        Ordering::Less => (i, j),
+        Ordering::Greater => (j ,i),
+    };
+
+    let (init, tail) = x.split_at_mut(hi);
+    mem::swap(&mut init[lo], &mut tail[0]);
+}
+
+// Sorts a subarray by quicksort
+// Input: Subarray of array a[0..n-1], defined by its left and right indices l and r
+// Output: Subarray a[l..r] sorted in nondecreasing order
+fn quicksort(a: &mut [u8], l: usize, r: usize) {
+    if l < r {
+        let s = hoare_partition(a, l, r);
+        quicksort(a, l, s - 1);
+        quicksort(a, s + 1, r);
+    }
+}
+
+// Partitions a subarray by Hoare's algorithm, using the first element as a pivot
+// Input: Subarray of array a[0..n-1], defined by its left and right indices l and r (l < r)
+// Output: Partition of a[l..r], with the split position returned as this function's value
+fn hoare_partition(a: &mut [u8], l: usize, r: usize) -> usize {
+    let p = a[l];
+    let mut i = l;
+    let mut j = r + 1;
+
+    while i < j {
+        loop {
+            i = i + 1;
+            if a[i] >= p {
+                break;
+            }
+        }
+        loop {
+            j = j - 1;
+            if a[j] <= p {
+                break;
+            }
+        }
+        swap(a, i, j);
+    }
+
+    swap(a, i, j); // undo last swap when i >= j
+    swap(a, l, j);
+
+    j
+}
+
 fn main() {
-    println!("Hello, world!");
+    // sort example array from book
+    let mut a: [u8; 8] = [5, 3, 1, 9, 8, 2, 4, 7];
+    println!("Array before quicksort: {:?}", a);
+
+    quicksort(&mut a, 0, 7);
+    println!("Array after quicksort: {:?}", a);
 }
