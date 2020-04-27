976cbf4c0682d0f75b3c2d4757dab709f7742ddc
Daniel Buckley <buckley.daniel.n@gmail.com>
2020-04-26 22:29:40 -0600

    Finish dynamic programming

--- dynamic-programming/mf-knapsack/src/main.rs
+++ dynamic-programming/mf-knapsack/src/main.rs
@@ -1,3 +1,48 @@
+extern crate matrix;
+
+use matrix::prelude::*;
+use std::vec::Vec;
+
+// Implements the memory function method for the knapsack problem
+// Input: A nonnegative integer i indicating the number of the first items
+//     being considered and a nonnegative integer j indicating the knapsack capacity
+// Output: The value of an optimal feasible subset of the first i items
+fn mf_knapsack(f: &mut Compressed<f64>, w: &Vec<f64>, v: &Vec<f64>, i: usize, j: usize) -> f64 {
+    if f.get((i, j)) < 0.0 {
+        let value;
+        if j < (w[i - 1] as usize) {
+            value = mf_knapsack(f, w, v, i - 1, j);
+        } else {
+            let l = mf_knapsack(f, w, v, i - 1, j);
+            let r = v[i - 1] + mf_knapsack(f, w, v, i - 1, j - (w[i - 1] as usize));
+            if l > r {
+                value = l;                
+            } else {
+                value = r;
+            }
+        }
+        f.set((i, j), value);
+    }
+    f.get((i, j))
+}
+
 fn main() {
-    println!("Hello, world!");
+    // initialize table values with -1 except for row 0 and column 0
+    let mut f = Compressed::<f64>::zero((5, 6));
+    for i in 1..5 {
+        for j in 1..6 {
+            f.set((i, j), -1.0);
+        }
+    }
+
+    // use example weights and values from figure 8.6 in book
+    let w: Vec::<f64> = vec![2.0, 1.0, 3.0, 2.0];
+    let v: Vec::<f64> = vec![12.0, 10.0, 20.0, 15.0];
+
+    println!("Weights: {:?}", w);
+    println!("Values: {:?}", v);
+    println!("Capacity W = 5");
+
+    let max_knapsack = mf_knapsack(&mut f, &w, &v, 4, 5);
+    println!("Maximum value of knapsack: {}", max_knapsack);
 }
--- dynamic-programming/optimal-binary-search-tree/src/main.rs
+++ dynamic-programming/optimal-binary-search-tree/src/main.rs
@@ -1,3 +1,62 @@
+extern crate matrix;
+
+use matrix::prelude::*;
+use std::vec::Vec;
+
+// Finds an optimal binary search tree by dynamic programming
+// Input: An array p[1..n] of search probabilities for a sorted list of n keys
+// Output: Average number of comparisons in successful searches in the
+//     optimal BST and table r of subtrees' roots in the optimal BST
+fn optimal_bst(p: &Vec<f64>) -> (f64, Compressed<usize>) {
+    let n = p.len();
+    let mut c = Compressed::<f64>::zero((n + 2, n + 1));
+    let mut r = Compressed::<usize>::zero((n + 2, n + 1));
+
+    for i in 1..(n + 1) {
+        c.set((i, i - 1), 0.0);
+        c.set((i, i), p[i - 1]);
+        r.set((i, i), i);
+    }
+    c.set((n + 1, n), 0.0);
+
+    // diagonal count
+    for d in 1..n {
+        for i in 1..(n - d + 1) {
+            let j = i + d;
+            let mut minval = f64::INFINITY;
+            let mut kmin = 0;
+            
+            for k in i..(j + 1) {
+                let c_sum = c.get((i, k - 1)) + c.get((k + 1, j));
+                if c_sum < minval {
+                    minval = c_sum;
+                    kmin = k;
+                }
+            }
+            r.set((i, j), kmin);
+            
+            let mut sum = p[i - 1];
+            for s in (i + 1)..(j + 1) {
+                sum = sum + p[s - 1];
+            }
+            c.set((i, j), minval + sum);
+        }
+    }
+
+    (c.get((1, n)), r)
+}
+
 fn main() {
-    println!("Hello, world!");
+    // use example search probabilities from book
+    let p: Vec::<f64> = vec![0.1, 0.2, 0.4, 0.3];
+    let opt = optimal_bst(&p);
+    println!("Average number of comparisons in successful searches: {}", opt.0);
+    
+    println!("Root table:");
+    for i in 1..opt.1.rows() {
+        for j in 0..opt.1.columns() {
+            print!("\t{}", opt.1.get((i, j)));
+        }
+        print!("\n");
+    }
 }
